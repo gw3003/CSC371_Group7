ac556d669a1b6602798b0523871fe62867c6b3f4
Gabe <gw3003@cs.ship.edu>
2020-05-01 15:21:57 -0400

    Add JDBC Jar to project

--- ProgrammingAssignment2/src/progAssign2/InsertStatements.java
+++ ProgrammingAssignment2/src/progAssign2/InsertStatements.java
@@ -1,5 +1,16 @@
 package progAssign2;
 
+/**
+ * @author Gabe Werick
+ * 
+ * This class has insert statements to add data to tables programming assignment2
+ *
+ */
 public class InsertStatements {
+	
+	public static void main(String[] args)
+	{
+		
+	}
 
 }
